87b32ea63ca8d55ecf25f4a8c94ffcce55ce29ae
MJ <manoj.kumar31028@gmail.com>
2022-06-13 22:55:02 -0500

    Initial commit

---
+++ ml_framework/experiment/experiment.py
@@ -0,0 +1,10 @@
+
+
+
+class Experiment:
+
+    def create_experiment(self):
+        pass
+
+    def run_experiment(self):
+        pass
---
+++ ml_framework/pipeline/pipeline.py
@@ -0,0 +1,10 @@
+
+
+
+class Pipeline:
+
+    def create_pipeline_from_steps(self, steps:list):
+        pass
+
+    def create_pipeline_from_config_json(self, json_config: dict):
+        pass
