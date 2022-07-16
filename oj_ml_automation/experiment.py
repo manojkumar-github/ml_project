4e634d446969e00b32467dbe2e39bfaeb3ace60c
Putchala <ManojKumar.Putchala.Contractor@pepsico.com>
2022-07-16 13:19:17 -0500

    added version capability

--- oj_ml_automation/experiment.py
+++ oj_ml_automation/experiment.py
@@ -12,10 +12,10 @@ from ml_project.ml_project.oj_ml_automation.pipeline.inference_pipeline import I
 
 class Experiment:
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, version:float):
         self.experiment_name, self.experiment_dir = \
             self._check_and_create_experiement(exp_name=name)
-        self.run_dir = self._create_run_dir()
+        self.run_dir = self._create_run_dir(version)
 
 
     @staticmethod
@@ -47,10 +47,20 @@ class Experiment:
         """
         return datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
-    def _create_run_dir(self):
+    def _generate_version_string(self, version_val):
+        return f"V_{'_'.join(str(float(version_val)).split('.'))}"
+
+    def _create_run_dir(self, version_val:float):
+        version_str = self._generate_version_string(version_val)
+        version_dir = os.path.join(self.experiment_dir, version_str)
+        if not os.path.isdir(version_dir):
+            ml_logger.info(f"{version_dir} version directory does not exist. Initiating it for the first time")
+            os.mkdir(version_dir)
+        else:
+            ml_logger.info(f"{version_dir} already exists. Skipping creation")
         run_name = self._generate_run_name()
-        run_dir_name = f"{run_name}_{self.experiment_name}"
-        run_dir = os.path.join(self.experiment_dir, run_dir_name)
+        run_dir_name = f"{run_name}_{version_str}_{self.experiment_name}"
+        run_dir = os.path.join(version_dir, run_dir_name)
         if not os.path.isdir(run_dir):
             ml_logger.info(f"{run_dir} run directory does not exist. Creating")
             os.mkdir(run_dir)
@@ -61,7 +71,7 @@ class Experiment:
         return run_dir
 
     def run(self, pipeline_obj: [TrainingPipeline, InferencePipeline]):
-        pipeline_obj.run(run_directory=run_dir)
+        pipeline_obj.run(run_directory=self.run_dir)
 
     def run_config(self, config: dict):
         pass
